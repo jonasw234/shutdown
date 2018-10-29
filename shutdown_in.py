2a2c39e00d72fc49934c11e0cee66dd114556c10
Jonas W <jonas_wendorf@web.de>
2018-10-30 00:49:04 +0100

    [+] Shutdown with time delta
    Also some refactoring.

--- shutdown_in.py
+++ shutdown_in.py
@@ -9,33 +9,48 @@ Usage:
     shutdown_in.py 25m
     shutdown_in.py 300s
     shutdown_in.py 20 (assumes seconds)
+    shutdown_in.py 2h-30m
 """
 import re
 import os
 import sys
 
 
+def to_int(time_element):
+    if time_element:
+        try:
+            return int(time_element[:-1])
+        except Exception:
+            return int(time_element)
+    return 0
+
+
+def usage():
+    print('Usage: shutdown_in.py 1h1m1s' \
+          '       shutdown_in.py 2h-10m')
+    return 1
+
+
 def main(argv):
     if len(argv) != 2:
-        print('Usage: shutdown_in.py 1h1m1s.')
-        return 1
-    time_string = re.match('(\d+h)?(\d+m)?(\d+s?)?', argv[1]).groups()
-    if time_string is not None:
-        seconds = 0
-        if time_string[0]: # hours
-            seconds += 60*60*int(time_string[0][:-1])
-        if time_string[1]: # minutes
-            seconds += 60*int(time_string[1][:-1])
-        if time_string[2]: # seconds
-            try:
-                seconds += int(time_string[2])
-            except Exception:
-                seconds += int(time_string[2][:-1])
+        return usage()
+    __import__('ipdb').set_trace()
+
+    time_string = ''.join(argv[1:])
+    time_groups = re.match('(\d+h)?(\d+m)?(\d+s?)?(\s*-\s*(\d+h)?(\d+m)?(\d+s?)?)?',
+                           time_string).groups()
+
+    if time_groups is not None:
+        seconds = 60*60*to_int(time_groups[0])  # Hours
+        seconds += 60*to_int(time_groups[1])  # Minutes
+        seconds += to_int(time_groups[2])  # Seconds
+        seconds -= 60*60*to_int(time_groups[4])  # Hours
+        seconds -= 60*to_int(time_groups[5])  # Minutes
+        seconds -= to_int(time_groups[6])  # Seconds
         os.system('powershell.exe -WindowStyle Hidden -Command "sleep {}; shutdown -s -f -t 0"'.format(seconds))
         return 0
     else:
-        print('Usage: shutdown_in.py 1h1m1s.')
-        return 1
+        return usage()
 
 
 if __name__ == '__main__':
